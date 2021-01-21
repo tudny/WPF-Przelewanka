d2476844a168570a4f35c8494d354096c2ef049b
tudny <aleksanderwt@gmail.com>
2021-01-21 09:25:25 +0100

    Replaced Hashtbl with Map module

--- przelewanka.ml
+++ przelewanka.ml
@@ -1,9 +1,16 @@
 
+module IntArrayMap = Map.Make( 
+  struct
+    type t = int array
+    let compare = compare
+  end
+);;
+
 exception Found of int
 
-let przelewanka (kubki : (int * int) array) : int =
+let przelewanka (cups : (int * int) array) : int =
 
-  let n = Array.length kubki
+  let n = Array.length cups
   in
 
   if n = 0 then 0 else
@@ -22,7 +29,7 @@ let przelewanka (kubki : (int * int) array) : int =
     (part fst, part snd)
   in
 
-  let (heights, after) = array_split kubki
+  let (heights, after) = array_split cups
   in
 
   let start n = Array.make n 0
@@ -36,63 +43,62 @@ let przelewanka (kubki : (int * int) array) : int =
   let can_be_done =
     Array.for_all (fun h -> h mod d = 0) after
     &&
-    Array.exists (fun (h, o) -> h = o || o = 0) kubki
+    Array.exists (fun (h, o) -> h = o || o = 0) cups
   in
 
   let q = Queue.create ()
   in
 
-  let vis = Hashtbl.create 1000
+  let vis = ref IntArrayMap.empty
   in
 
-  let do_pelna x stan = 
-    let stan = Array.copy stan in
-    stan.(x) <- heights.(x);
-    stan
+  let until_full x state = 
+    let state = Array.copy state in
+    state.(x) <- heights.(x);
+    state
   in
 
-  let od_pusta x stan = 
-    let stan = Array.copy stan in
-    stan.(x) <- 0;
-    stan
+  let until_empty x state = 
+    let state = Array.copy state in
+    state.(x) <- 0;
+    state
   in
 
   (* Przelej x do y *)
-  let przelej stan_def y x =
-    let stan = Array.copy stan_def in 
-    let left = heights.(y) - stan.(y) in 
-    if left >= stan.(x) then begin
-      stan.(y) <- stan.(y) + stan.(x);
-      stan.(x) <- 0;
+  let pour state_def y x =
+    let state = Array.copy state_def in 
+    let left = heights.(y) - state.(y) in 
+    if left >= state.(x) then begin
+      state.(y) <- state.(y) + state.(x);
+      state.(x) <- 0;
     end else begin
-      stan.(y) <- heights.(y);
-      stan.(x) <- stan.(x) - left
+      state.(y) <- heights.(y);
+      state.(x) <- state.(x) - left
     end;
-    stan
+    state
   in
 
   Queue.add (start n) q;
 
-  Hashtbl.add vis (start n) 0;
+  vis := IntArrayMap.add (start n) 0 !vis;
 
   try 
     while Queue.is_empty q |> not && can_be_done do
       let front = Queue.take q in
-      let dis = Hashtbl.find vis front in
-      flush stdout;
+      let dis = IntArrayMap.find front !vis in
       if front = after then raise (Found dis);
       let process new_way = 
-        if Hashtbl.mem vis new_way |> not then begin
-          Hashtbl.add vis new_way (dis + 1);
+        if IntArrayMap.mem new_way !vis |> not then begin
+          vis := IntArrayMap.add new_way (dis + 1) !vis;
           Queue.add new_way q
         end
       in
       Array.iteri (fun ele va ->
-          if va <> 0 then process (od_pusta ele front);
-          if va <> heights.(ele) then process (do_pelna ele front); 
+          if va <> 0 then process (until_empty ele front);
+          if va <> heights.(ele) then process (until_full ele front); 
           Array.iteri (fun ele2 _ -> 
               if ele <> ele2 && heights.(ele2) <> 0 && heights.(ele) <> va then begin
-                process (przelej front ele ele2)
+                process (pour front ele ele2)
               end 
             ) front;
         ) front
